394677151234e21b41a4c47ae42b71afa2ec520b
cice <cice@A8POVMac11.local>
2018-06-18 21:31:00 +0200

    ejercicio 1

--- src/Cadenas.java
+++ src/Cadenas.java
@@ -1,2 +1,36 @@
+import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
+
+import java.util.Scanner;
+
 public class Cadenas {
+
+    public static void main(String[] args) {
+
+
+
+        String[] array = new String[4];
+        Scanner sc = new Scanner(System.in);
+        for (int i=0; i<4; i++){
+            System.out.println("Introduce una cadena");
+            String st = sc.nextLine();
+            array[i] = st;
+        }
+
+        int max = 0;
+        int position = 0;
+        for(int i=0; i<array.length;i++){
+
+            if (array[i].length() > max){
+                max =array[i].length();
+                position = i;
+            }
+
+        }
+
+        System.out.println("la cadena mayor esta en la posicion " + position + " del array y tiene " + max + " caracteres.");
+
+
+
+
+    }
 }
